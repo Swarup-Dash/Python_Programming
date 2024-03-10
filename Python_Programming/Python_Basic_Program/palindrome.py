56fbb60e31c426e384cc2ddd00db05e1538391cf
Swarup Dash <146019694+Swarup-Dash@users.noreply.github.com>
2024-03-10 12:37:16 +0530

    Add files via upload

---
+++ Python_Programming/Python_Basic_Program/palindrome.py
@@ -0,0 +1,12 @@
+# Palindrome: Number is same as its reverse i.e 2332 reverse is also 2332
+n=int(input("Enter a number"))
+temp=n
+reverse=0
+while temp>0:
+    remainder=temp%10
+    reverse=(reverse*10)+remainder
+    temp=temp//10
+if n==reverse:
+    print(n,"is palindrome")
+else:
+    print(n,"is not palindrme")
